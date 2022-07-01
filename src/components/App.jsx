83011828008a8fb1d66b4c73db12d5ef0348d195
Aditya Kumar <55875838+kakarot2019@users.noreply.github.com>
2022-07-02 02:12:03 +0530

    Added arrow functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import emojipedia from "../emojipedia";
 import Entry from "./Entry";
 
 //creating a func() to generalize data that can be sent over.
-function creatEntry(emojiTerm) {
+/*function creatEntry(emojiTerm) {
   return (
     <Entry
       //calling to all js object from emojipedia
@@ -13,9 +13,10 @@ function creatEntry(emojiTerm) {
       description={emojiTerm.meaning}
     />
   );
-}
+}*/
 
 //using map is like using loops with function as parameter (here creatEntry)
+//we can alse use arrow function for direct call
 function App() {
   return (
     <div>
@@ -23,7 +24,17 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(creatEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojiTerm) => (
+          <Entry
+            //calling to all js object from emojipedia
+            key={emojiTerm.id} //key == unique id which is hidden
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
